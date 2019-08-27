fdde4ccbb6c8fb268a9529343c965d0f389a9068
PhamJimmy <PhhJimmy@gmail.com>
2019-08-26 18:27:28 -0700

    created basic parent templates

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    # Current topic user is interested in
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        #returns string representation of self
+        return self.text
+
+class Entry(models.Model):
+    # Information about topic
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        #returns string representation of self
+        return f"{self.text[:50]}..."
---
+++ learning_logs/urls.py
@@ -0,0 +1,10 @@
+# Defines URl patterns for learning_logs
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+]
